62c81e80cd7ece8d9803490752cacfedfa2982c5
szacio <63015226+szacio@users.noreply.github.com>
2020-04-21 03:57:58 +0200

    Sudoku change 1.0

--- src/com/karolstudent/sudoku2_0/Sudoku.java
+++ src/com/karolstudent/sudoku2_0/Sudoku.java
@@ -4,6 +4,11 @@ package com.karolstudent.sudoku2_0;
  * Created by Karol on 2020-04-20
  */
 public class Sudoku {
+    public boolean checkXY(int X, int Y){
+        System.out.println("Check attempted here");
+        if (X == 1 && Y == 1) return true;
+        else return false;
+    }
 
 
 }
